f50a4f7d725de514cfd4b9c7fd4252cd5fcbe1fd
Çınar Özgürgil <cozgurgil@gmail.com>
2025-05-23 20:11:08 +0300

    Details-Finished

--- BlogUygulaması/Controllers/WordDetails.cs
+++ BlogUygulaması/Controllers/WordDetails.cs
@@ -1,13 +1,24 @@
+using System.Threading.Tasks;
+using BlogUygulaması.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace BlogUygulaması.Controllers;
 
 public class WordDetails : Controller
 {
+    private readonly DataContext _context;
 
-    public ActionResult Index()
+    public WordDetails(DataContext context)
     {
-        return View();
+        _context = context;
+    }
+
+    public async Task<ActionResult> Index(int Id)
+    {
+        var result = await _context.WordModels.Where(i => i.Id == Id).FirstOrDefaultAsync();
+
+        return View(result);
     }
 
 }
