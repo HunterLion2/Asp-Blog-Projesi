122f298b9a229347cf5840c160b0ab25efe5ebce
Çınar Özgürgil <cozgurgil@gmail.com>
2025-06-06 18:29:08 +0300

    login-disagn-completed

--- BlogUygulaması/obj/Debug/net9.0/BlogUygulaması.AssemblyInfo.cs
+++ BlogUygulaması/obj/Debug/net9.0/BlogUygulaması.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlogUygulaması")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26767edd3f762b0444521eb2073e3048459c59e7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+90cef2037d0a5acb32ca89ea6d585ce25ff5226b")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlogUygulaması")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlogUygulaması")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
